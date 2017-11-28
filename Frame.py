2bc0686a27bc19020decf360ff2be9edb407079d
Sam West <31940432+Samjwest5@users.noreply.github.com>
2017-11-27 22:18:47 -0600

    Update Frame.py

--- Frame.py
+++ Frame.py
@@ -71,7 +71,7 @@ class classifiedRaster: #class definition for the frames made from the whole ras
 					array = arcpy.Array([arcpy.Point(x, y), arcpy.Point(x, y + self.__frameY),arcpy.Point(x + self.__frameX, y + self.__frameY),arcpy.Point(x + self.__frameX, y)]) #creating the frame polygon
 					polygon = arcpy.Polygon(array)
 		          
-					cursor.insertRow([polygon,validRatio, float(x+(x + self.__frameX)/2),float(y+(y + self.__frameY)/2))]) #add frame to feature class
+					cursor.insertRow([polygon,validRatio, float(x+(x + self.__frameX)/2),float(y+(y + self.__frameY)/2)]) #add frame to feature class
 
 					x += self.__frameX #adjust counter for positive condition
 					continue #back to beginning of while loop
