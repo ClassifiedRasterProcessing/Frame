2d2a1f0a9d88614192f438e8ec92d259a00e9a9e
Sam West <31940432+Samjwest5@users.noreply.github.com>
2017-11-29 17:23:09 -0600

    Update Frame.py
    Fixed some odd tabulations
    Potential fix for the total frames calculation
    Added graduated symbology using breakpoints and the ratio values, as well as a slight transparency (not confirmed working)

--- Frame.py
+++ Frame.py
@@ -19,115 +19,117 @@ class classifiedRaster: #class definition for the frames made from the whole ras
 
 		
     def processRaster(self,output, User_Field_Count, Class_List, User_Field, Fields_List):
-		arcpy.AddMessage("Processing raster.")
-		fc = output
+	arcpy.AddMessage("Processing raster.")
+	fc = output
+	arcpy.env.overwriteOutput = True
 
-		arcpy.env.overwriteOutput = True
-
-		#arcpy.AddMessage(str(arcpy.env.workspace) +"  "+ str(os.path.split(output)[1]))
-		#arcpy.AddMessage(str(fc))
-		#arcpy.AddMessage(arcpy.Exists(output))
-		#arcpy.AddMessage(str(output))
+	#arcpy.AddMessage(str(arcpy.env.workspace) +"  "+ str(os.path.split(output)[1]))
+	#arcpy.AddMessage(str(fc))
+	#arcpy.AddMessage(arcpy.Exists(output))
+	#arcpy.AddMessage(str(output))
 		
-		arcpy.management.CreateFeatureclass(arcpy.env.workspace,os.path.split(output)[1],"POLYGON")
-		frame = "TempClip"#defining the location where the temporary frame will be saved       might need arcpy.env.workspace + 
-		#may need a \\ before temp
+	arcpy.management.CreateFeatureclass(arcpy.env.workspace,os.path.split(output)[1],"POLYGON")
+	frame = "TempClip"#defining the location where the temporary frame will be saved
 		
-		R="Ratio"
-		F="FLOAT"
-		arcpy.management.AddField(fc,R,F)
-		arcpy.management.AddField(fc,"X",F)
-		arcpy.management.AddField(fc,"Y",F)
-		#arcpy.management.AddField("frame_ratio","FRAME_ID","SHORT")
-		projection = arcpy.Describe(self.__inras).spatialReference
-		arcpy.DefineProjection_management(fc,projection)
-		cursor = arcpy.da.InsertCursor(fc, ["SHAPE@","Ratio","X","Y"]) #cursor for creating the valid frame feature class
-		#arcpy.AddMessage("Passed the cursor")
-		y = float(self.__min_y) #set to bottom of in raster
+	R="Ratio"
+	F="FLOAT"
+	arcpy.management.AddField(fc,R,F) #adding properties to attribute table
+	arcpy.management.AddField(fc,"X",F)
+	arcpy.management.AddField(fc,"Y",F)
+	projection = arcpy.Describe(self.__inras).spatialReference
+	arcpy.DefineProjection_management(fc,projection)
+	cursor = arcpy.da.InsertCursor(fc, ["SHAPE@","Ratio","X","Y"]) #cursor for creating the valid frame feature class
+	#arcpy.AddMessage("Passed the cursor")
+	y = float(self.__min_y) #set to bottom of in raster
 		
-		frameCount = 0 #some nice counters for output while prcessing
-		validFrameCount = 0
-		totalFrames = int(((self.__max_y-self.__min_y)/self.__frameY*2) * ((self.__max_x-self.__min_x)/self.__frameX*2))
+	frameCount = 0 #some nice counters for output while processing
+	validFrameCount = 0
+	totalFrames = int(((self.__max_y-self.__min_y)/self.__frameY*2+1) * ((self.__max_x-self.__min_x)/self.__frameX*2+1))
 		
-		#arcpy.AddMessage("y = " +str(y))
-		#arcpy.AddMessage("max Y = " +str(self.__max_y))
-		while(y < self.__max_y):#flow control based on raster size and requested frame size needed. Issue on edges, ask about.
-			x = float(self.__min_x) #set to left bound of in raster
-			#arcpy.AddMessage("Passed 1 while")
-			#arcpy.AddMessage("x = " +str(x))
-			#arcpy.AddMessage("max X = " +str(self.__max_x))
-			while (x < self.__max_x): #"side to side" processing
-				#arcpy.AddMessage("Passed 2 while")
-				rectangle = str(x) + " " + str(y) + " " + str(x + self.__frameX) + " " + str(y + self.__frameY) #bounds of our frame for the clip tool
-				#arcpy.AddMessage("Current rectangle: " + str(rectangle))
-				arcpy.Clip_management(self.__inras,rectangle, frame)#create frame -> clip out a section of the main raster 
-                		frameCount += 1
-				arcpy.AddMessage("Processing frame #" + str(frameCount) + " out of " + str(totalFrames))
+	#arcpy.AddMessage("y = " +str(y))
+	#arcpy.AddMessage("max Y = " +str(self.__max_y))
+	while(y < self.__max_y):#flow control based on raster size and requested frame size needed. Issue on edges, ask about.
+		x = float(self.__min_x) #set to left bound of in raster
+		#arcpy.AddMessage("Passed 1 while")
+		#arcpy.AddMessage("x = " +str(x))
+		#arcpy.AddMessage("max X = " +str(self.__max_x))
+		while (x < self.__max_x): #"side to side" processing
+			#arcpy.AddMessage("Passed 2 while")
+			rectangle = str(x) + " " + str(y) + " " + str(x + self.__frameX) + " " + str(y + self.__frameY) #bounds of our frame for the clip tool
+			#arcpy.AddMessage("Current rectangle: " + str(rectangle))
+			arcpy.Clip_management(self.__inras,rectangle, frame)#create frame -> clip out a section of the main raster 
+               		frameCount += 1 #updating processing counter
+			arcpy.AddMessage("Processing frame #" + str(frameCount) + " out of " + str(totalFrames))
 				
-				validFrame, validRatio = density(frame, self.__frame_ratio, self.__in_class, User_Field_Count, Class_List, User_Field, Fields_List) #run ratio function. Expect boolean T if frame meets ratio conditions, and actual ratio
-				if validFrame: #Case it passes
-					arcpy.AddMessage("Valid frame.")
-					validFrameCount += 1
-					array = arcpy.Array([arcpy.Point(x, y), arcpy.Point(x, y + self.__frameY),arcpy.Point(x + self.__frameX, y + self.__frameY),arcpy.Point(x + self.__frameX, y)]) #creating the frame polygon
-					polygon = arcpy.Polygon(array)
-		          		lat = y+self.__frameY/2
-					long = x+self.__frameX/2
-					cursor.insertRow([polygon,validRatio, lat, long]) #add frame to feature class
+			validFrame, validRatio = density(frame, self.__frame_ratio, self.__in_class, User_Field_Count, Class_List, User_Field, Fields_List) #run ratio function. Expect boolean T if frame meets ratio conditions, and actual ratio
+			if validFrame: #Case it passes
+				arcpy.AddMessage("Valid frame.")
+				validFrameCount += 1
+				array = arcpy.Array([arcpy.Point(x, y), arcpy.Point(x, y + self.__frameY),arcpy.Point(x + self.__frameX, y + self.__frameY),arcpy.Point(x + self.__frameX, y)]) #creating the frame polygon
+				polygon = arcpy.Polygon(array)
+		          	lat = y+self.__frameY/2
+				long = x+self.__frameX/2
+				cursor.insertRow([polygon,validRatio, lat, long]) #add frame to feature class with calculated attributes
 
-					x += self.__frameX #adjust counter for positive condition
-					continue #back to beginning of while loop
+				x += self.__frameX #adjust counter for positive condition
+				continue #back to beginning of while loop
 
-				x = int(x) + int(float(self.__frameX)//2)#move half a frame "right"...case when previous frame invalid "Fast option"
-			y = float(y) + int(float(self.__frameY)//2)#move half a frame "up" ... "Fast option"
-			
-		del cursor #prevent data corruption by deleting cursor when finished
-		arcpy.AddMessage("Finished processing raster. " + str(validFrameCount) + " valid frames found.")
+			x = int(x) + int(float(self.__frameX)//2)#move half a frame "right"...case when previous frame invalid "Fast option"
+		y = float(y) + int(float(self.__frameY)//2)#move half a frame "up" ... "Fast option"	
+	del cursor #prevent data corruption by deleting cursor when finished
 		
+	sym = fc.symbology
+	if hasattr(sym, 'renderer'): #draws layer with graduated symbology using 5 breakpoints, a color ramp, and slight transparency
+		sym.updateRenderer('GraduatedColorsRenderer')
+		sym.colorizer.classificationField = R
+		sym.renderer.breakCount = 5
 		
+		p = arcpy.mp.ArcGISProject("CURRENT")#determining color ramp
+		colorRamp = p.listColorRamps("White to Black")[0] #Check with Crockett to see which settings look best
+		sym.renderer.colorRamp = colorRamp
 		
-def density(inras, ratio, inclass, User_Field_Count, Class_List, User_Field_Value,Fields_List): #added the needed inputs
+		map_lyr = arcpy.mapping.Layer(arcpy.env.workspace + os.path.split(output)[1] + ".lyr")#file path of the feature class layer file
+		map_lyr.transparency = 30# 30% transparency 
+		
+		fc.symbology = sym #assign symbology to vector output
+		#p.saveACopy(relpath + r'\\SavedOutput.aprx') #not sure if needed, would adjust save path
+		
+	arcpy.AddMessage("Finished processing raster. " + str(validFrameCount) + " valid frames found.")
+				
+		
+def density(inras, ratio, inclass, User_Field_Count, Class_List, User_Field_Value,Fields_List): #determines ratio of classification
 	fc = inras #Determines file path from user input
 	#arcpy.AddMessage("Processing frame.")
 	#arcpy.AddMessage("fc = " + str(fc))
 	
 	countField= User_Field_Count
 	arcpy.BuildRasterAttributeTable_management(fc, "Overwrite") #updates attribute table to reflect frame, rather than whole
-	#F="FLOAT"
-	#arcpy.management.AddField(fc,field,F) #creating attribute table to store frequencies in
 	cursor = arcpy.SearchCursor(fc,Fields_List)
 	
-	frequency = 0
+	frequency = 0 #counters
 	total = 0
-	#CountColumn = 0
-	#ValueColumn = 0
-	#i = 0
-	#for column in Fields_List: #determines which column contains the counts for data retrieval
-	#	if column == User_Field_Count:
-	#		CountColumn = i
-	#	if column == User_Field_Value:
-	#		ValueColumn = i
-	#	i += 1	
-	
+
 	for row in cursor: #Calculates information on each classification
-		arcpy.AddMessage("inclass = " + str(inclass))
-		arcpy.AddMessage("row.getValue(User_Field_Value) = " + str(row.getValue(User_Field_Value)))
+		#arcpy.AddMessage("inclass = " + str(inclass))
+		#arcpy.AddMessage("row.getValue(User_Field_Value) = " + str(row.getValue(User_Field_Value)))
 		#arcpy.AddMessage("row[ValueColumn] = " + str(row[ValueColumn]))
 		try:
 			#if row[ValueColumn] == inclass: #calc frequency of the classification requested
 			if int(row.getValue(User_Field_Value)) == int(inclass): #calc frequency of the classification requested
 				frequency = row.getValue(User_Field_Count)
 				arcpy.AddMessage("Frequency = " + str(row.getValue(User_Field_Count)))
-			total += row.getValue(User_Field_Count) #calc sum
+			total += row.getValue(User_Field_Count) #calculates sum
 			arcpy.AddMessage("Total = " + str(total))
 		except:
 			arcpy.AddMessage("Not in frame.")
 			return False, 0
+		
 	if total == 0: #preventing dividing by 0, case where there is nothing in the frame
-		arcpy.AddMessage("Frame empty.")
+		arcpy.AddMessage("Frame empty.") #Potentially adjust angle of rectangle to match that of the raster, would save processing
 		return False, 0
 	
 	final_ratio = float(frequency)/float(total) #Calculates ratio for user input classification
-	arcpy.AddMessage("Frame has density " + str(final_ratio))
+	arcpy.AddMessage("Frame has density = " + str(final_ratio))
 	if final_ratio >= ratio: 
 		return True, final_ratio #Returns true and final ratio if user input is met
 	else:
